fc9a91680ed85229aa390ec8d63a097ed10d5247
Қонысбек Қуаныш <00057394@nb>
2025-03-24 14:12:46 +0500

    Добавление terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Category from './pages/Category';
-import CarRental from './pages/CarRental';
-import './App.css';
+import TerminalComponent from './components/TerminalComponent';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/car_rental" element={<CarRental />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <div style={{ height: '100vh', backgroundColor: '#000' }}>
+      <TerminalComponent />
+    </div>
   );
 }
 
