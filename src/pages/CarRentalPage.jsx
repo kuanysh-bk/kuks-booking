e3febee809accd07c713f7815a4081a26d7ba01b
Қонысбек Қуаныш <00057394@nb>
2025-05-26 16:33:44 +0500

    update filters in cars page

--- src/pages/CarRentalPage.jsx
+++ src/pages/CarRentalPage.jsx
@@ -10,23 +10,15 @@ const CarRentalPage = () => {
   const [showFilters, setShowFilters] = useState(false);
 
   const [filter, setFilter] = useState({
-    type: '',
-    brand: '',
-    model: '',
-    seatsFrom: '',
-    seatsTo: '',
-    priceFrom: '',
-    priceTo: '',
-    yearFrom: '',
-    yearTo: '',
-    engineFrom: '',
-    engineTo: '',
-    mileageFrom: '',
-    mileageTo: ''
+    type: '', brand: '', model: '', color: '',
+    transmission: '', airConditioning: '', fuel_type: '', drive_type: '',
+    seatsFrom: '', seatsTo: '', priceFrom: '', priceTo: '',
+    yearFrom: '', yearTo: '', engineFrom: '', engineTo: '', mileageFrom: '', mileageTo: ''
   });
 
   const [brands, setBrands] = useState([]);
   const [models, setModels] = useState([]);
+  const [allModels, setAllModels] = useState([]);
 
   useEffect(() => {
     fetch('https://booking-backend-tjmn.onrender.com/cars')
@@ -34,18 +26,20 @@ const CarRentalPage = () => {
       .then(data => {
         setCars(data);
         const brandSet = new Set();
-        const modelSet = new Set();
+        const modelMap = new Map();
         let minSeats = Infinity, maxSeats = 0;
 
         data.forEach(car => {
           brandSet.add(car.brand);
-          modelSet.add(car.model);
+          if (!modelMap.has(car.brand)) modelMap.set(car.brand, new Set());
+          modelMap.get(car.brand).add(car.model);
           if (car.seats < minSeats) minSeats = car.seats;
           if (car.seats > maxSeats) maxSeats = car.seats;
         });
 
         setBrands(Array.from(brandSet));
-        setModels(Array.from(modelSet));
+        setAllModels(modelMap);
+        setModels([]);
 
         setFilter(f => ({
           ...f,
@@ -61,6 +55,14 @@ const CarRentalPage = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (filter.brand && allModels.has(filter.brand)) {
+      setModels(Array.from(allModels.get(filter.brand)));
+    } else {
+      setModels([]);
+    }
+  }, [filter.brand, allModels]);
+
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
     setFilter(prev => ({ ...prev, [name]: value }));
@@ -70,18 +72,20 @@ const CarRentalPage = () => {
 
   const filteredCars = cars.filter(car => {
     const {
-      type, brand, model,
-      seatsFrom, seatsTo,
-      priceFrom, priceTo,
-      yearFrom, yearTo,
-      engineFrom, engineTo,
-      mileageFrom, mileageTo
+      type, brand, model, color, transmission, airConditioning, fuel_type, drive_type,
+      seatsFrom, seatsTo, priceFrom, priceTo,
+      yearFrom, yearTo, engineFrom, engineTo, mileageFrom, mileageTo
     } = filter;
 
     return (
       (!type || car.car_type === type) &&
       (!brand || car.brand === brand) &&
       (!model || car.model === model) &&
+      (!color || car.color === color) &&
+      (!transmission || car.transmission === transmission) &&
+      (!airConditioning || (airConditioning === 'yes' ? car.has_air_conditioning : !car.has_air_conditioning)) &&
+      (!fuel_type || car.fuel_type === fuel_type) &&
+      (!drive_type || car.drive_type === drive_type) &&
       (!seatsFrom || car.seats >= parseInt(seatsFrom)) &&
       (!seatsTo || car.seats <= parseInt(seatsTo)) &&
       (!priceFrom || car.price_per_day >= parseFloat(priceFrom)) &&
@@ -102,6 +106,16 @@ const CarRentalPage = () => {
       <button className="toggle-filters" onClick={toggleFilters}>
         {showFilters ? t('cars.hideFilters') : t('cars.showFilters')}
       </button>
+      <button className="reset-filters" onClick={() => {
+        setFilter({
+          type: '', brand: '', model: '', color: '',
+          transmission: '', airConditioning: '', fuel_type: '', drive_type: '',
+          seatsFrom: '', seatsTo: '', priceFrom: '', priceTo: '',
+          yearFrom: '', yearTo: '', engineFrom: '', engineTo: '', mileageFrom: '', mileageTo: ''
+        });
+      }}>
+        {t('cars.resetFilters')}
+      </button>
 
       {showFilters && (
         <div className="advanced-filters">
@@ -117,9 +131,48 @@ const CarRentalPage = () => {
             {brands.map(b => <option key={b} value={b}>{b}</option>)}
           </select>
 
-          <select name="model" value={filter.model} onChange={handleFilterChange}>
-            <option value="">{t('cars.filters.allModels')}</option>
-            {models.map(m => <option key={m} value={m}>{m}</option>)}
+          {filter.brand && (
+            <select name="model" value={filter.model} onChange={handleFilterChange}>
+              <option value="">{t('cars.filters.allModels')}</option>
+              {models.map(m => <option key={m} value={m}>{m}</option>)}
+            </select>
+          )}
+
+          <select name="color" value={filter.color} onChange={handleFilterChange}>
+            <option value="">{t('cars.filters.allColors')}</option>
+            <option value="white">{t('cars.colors.white')}</option>
+            <option value="black">{t('cars.colors.black')}</option>
+            <option value="silver">{t('cars.colors.silver')}</option>
+            <option value="gray">{t('cars.colors.gray')}</option>
+            <option value="blue">{t('cars.colors.blue')}</option>
+            <option value="red">{t('cars.colors.red')}</option>
+          </select>
+
+          <select name="transmission" value={filter.transmission} onChange={handleFilterChange}>
+            <option value="">{t('cars.filters.allTransmissions')}</option>
+            <option value="automatic">{t('cars.transmissionTypes.automatic')}</option>
+            <option value="manual">{t('cars.transmissionTypes.manual')}</option>
+          </select>
+
+          <select name="airConditioning" value={filter.airConditioning} onChange={handleFilterChange}>
+            <option value="">{t('cars.filters.anyAC')}</option>
+            <option value="yes">{t('common.yes')}</option>
+            <option value="no">{t('common.no')}</option>
+          </select>
+
+          <select name="fuel_type" value={filter.fuel_type} onChange={handleFilterChange}>
+            <option value="">{t('cars.filters.allFuels')}</option>
+            <option value="petrol">{t('cars.fuel.petrol')}</option>
+            <option value="diesel">{t('cars.fuel.diesel')}</option>
+            <option value="gas">{t('cars.fuel.gas')}</option>
+            <option value="electric">{t('cars.fuel.electric')}</option>
+          </select>
+
+          <select name="drive_type" value={filter.drive_type} onChange={handleFilterChange}>
+            <option value="">{t('cars.filters.allDrives')}</option>
+            <option value="fwd">{t('cars.drive.fwd')}</option>
+            <option value="rwd">{t('cars.drive.rwd')}</option>
+            <option value="awd">{t('cars.drive.awd')}</option>
           </select>
 
           <div className="range-group">
