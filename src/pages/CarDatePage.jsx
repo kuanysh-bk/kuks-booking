2eabfd7e77419c769742bba9bafd2a31423724c4
Қонысбек Қуаныш <00057394@nb>
2025-05-28 11:50:41 +0500

    debuging unavailableDates in CarDatePage

--- src/pages/CarDatePage.jsx
+++ src/pages/CarDatePage.jsx
@@ -42,6 +42,8 @@ const CarDatePage = () => {
         setUnavailableDates(dates);
       })
       .catch(err => console.error('Ошибка загрузки дат:', err));
+      console.log('Недоступные даты:', unavailableDates);
+    
   }, [carId]);
 
   const handleContinue = () => {
@@ -59,8 +61,6 @@ const CarDatePage = () => {
       },
     });
   };
-
-  console.log('Недоступные даты:', unavailableDates);
 
   return (
     <div className="date-page-wrapper">
